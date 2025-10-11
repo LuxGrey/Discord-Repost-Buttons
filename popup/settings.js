ed4d0d026478a27abec5ceeed129e9db83da86dd
LuxGrey <>
2025-10-11 15:21:41 +0200

    Add settings popup for configuring repost server parameters
    This allows the user to specify the API-endpoint that repost requests should be made to
    as well as an auth secret that will be delivered with requests to the repost server.

---
+++ popup/settings.js
@@ -0,0 +1,36 @@
+const repostServerRedditUrlInput = document.getElementById('repostServerRedditUrl');
+const authTokenInput = document.getElementById('authToken');
+const toggleButton = document.getElementById('toggleAuthTokenVisibility');
+const saveButton = document.getElementById('save');
+const saveMessage = document.getElementById('saveMessage');
+
+// toggle visibility of value in authToken input field when clicking button
+toggleButton.addEventListener('click', () => {
+    const isPassword = authTokenInput.type === 'password';
+    authTokenInput.type = isPassword ? 'text' : 'password';
+    toggleButton.textContent = isPassword ? '🙈' : '👁️';
+    toggleButton.setAttribute('aria-label', isPassword ? 'Hide password' : 'Show password');
+});
+
+saveButton.addEventListener('click', () => {
+    const repostServerRedditUrl = repostServerRedditUrlInput.value;
+    const authToken = authTokenInput.value;
+    browser.storage.sync.set({ repostServerRedditUrl, authToken }).then(() => {
+        saveMessage.textContent = "✅ Settings saved";
+        saveMessage.classList.remove('hidden');
+
+        setTimeout(() => {
+            saveMessage.classList.add('hidden');
+        }, 3000);
+    });
+});
+
+// load stored values
+browser.storage.sync.get(['repostServerRedditUrl', 'authToken']).then((result) => {
+    if (result.repostServerRedditUrl) {
+        repostServerRedditUrlInput.value = result.repostServerRedditUrl;
+    }
+    if (result.authToken) {
+        authTokenInput.value = result.authToken;
+    }
+});
